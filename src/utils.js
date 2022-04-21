e45a553427dc2be1d49086374ed3a55ac67b8ec8
DanielMa <mwldaniel_008@126.com>
2022-04-21 11:45:37 +0800

    Building Board and Opening Tasks in a Modal

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
-export function uuid () {
-  return Math.random().toString(16).slice(2)
+/* eslint-disable */
+
+export function uuid() {
+  return Math.random()
+    .toString(16)
+    .slice(2)
 }
 
-export function saveStatePlugin (store) {
-  store.subscribe(
-    (mutation, state) => localStorage.setItem('boards', JSON.stringify(state.boards))
+export function saveStatePlugin(store) {
+  store.subscribe((mutation, state) =>
+    localStorage.setItem('board', JSON.stringify(state.board))
   )
 }
